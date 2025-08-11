0f6211b3788b5e5ac1d1c11481d54963784004b9
dickysaputra2006 <153079528+dickysaputra2006@users.noreply.github.com>
2025-08-11 19:02:45 +0700

    fix token

--- backend/botTele.js
+++ backend/botTele.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = '7959249633:AAEICw51jXEixSFfOpY4E1H8rzXL29A3nFI'; // Ganti dengan token bot Telegram Anda
 const adminApiKey = process.env.ADMIN_API_KEY;
 const apiUrl = 'http://localhost:3000';
 
